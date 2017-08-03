4ccc17875e9a6a391d9fc3c139d989fe09b98471
Lev Aronsky <aronsky@gmail.com>
2017-08-03 18:11:09 +0300

    Continued work on generic fan.

--- homeassistant/components/fan/generic_fan.py
+++ homeassistant/components/fan/generic_fan.py
@@ -14,7 +14,8 @@ from homeassistant.components.fan import (SPEED_LOW, SPEED_MEDIUM, SPEED_HIGH,
                                           FanEntity, SUPPORT_SET_SPEED,
                                           SUPPORT_OSCILLATE, SUPPORT_DIRECTION,
                                           PLATFORM_SCHEMA)
-from homeassistant.const import (STATE_OFF, STATE_ON, CONF_DEVICES, CONF_NAME,
+from homeassistant.const import (STATE_UNKNOWN, STATE_OFF, STATE_ON,
+                                 CONF_DEVICES, CONF_NAME,
                                  EVENT_HOMEASSISTANT_START)
 from homeassistant.helpers.event import async_track_state_change
 from homeassistant.helpers.restore_state import async_get_last_state
@@ -27,11 +28,15 @@ CONF_SET_SPEED = "set_speed"
 CONF_CHANGE_SPEED = "change_speed"
 CONF_OSCILLATION_SWITCH = "oscillation"
 CONF_SET_DIRECTION = "set_direction"
+CONF_MEMORY = "memory"
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_NAME): cv.string,
     vol.Required(CONF_POWER_SWITCH): cv.entity_id,
     vol.Optional(CONF_OSCILLATION_SWITCH, default=None): cv.entity_id,
+    vol.Optional(CONF_SET_SPEED, default=None): cv.SCRIPT_SCHEMA,
+    vol.Optional(CONF_SET_DIRECTION, default=None): cv.SCRIPT_SCHEMA,
+    vol.Optional(CONF_MEMORY, default=False): cv.boolean,
 })
 
 # pylint: disable=unused-argument
@@ -39,9 +44,13 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 def async_setup_platform(hass, config, async_add_devices, discovery_info=None):
     """Set up the generic fan platform."""
     async_add_devices([GenericFan(
+        hass,
         config.get(CONF_NAME),
         config.get(CONF_POWER_SWITCH),
-        config.get(CONF_OSCILLATION_SWITCH)
+        config.get(CONF_OSCILLATION_SWITCH),
+        config.get(CONF_SET_SPEED),
+        config.get(CONF_SET_DIRECTION),
+        config.get(CONF_MEMORY)
         )
     ], True)
 
@@ -49,13 +58,31 @@ def async_setup_platform(hass, config, async_add_devices, discovery_info=None):
 class GenericFan(FanEntity):
     """A generic fan component."""
 
-    def __init__(self, name: str, power_switch: str,
-                 oscillation_switch: str) -> None:
+    def __init__(self, hass, name: str, power_switch: str,
+                 oscillation_switch: str, set_speed_script: str,
+                 set_direction_script: str, memory: bool) -> None:
         """Initialize the generic fan."""
         self._name = name
+        
         self._power_switch = power_switch
         self._oscillation_switch = oscillation_switch
-        self._state = None
+        self._set_speed_script = Script(hass, set_speed_script) \
+            if set_speed_script else None
+        self._set_direction_script = Script(hass, set_direction_script) \
+            if set_direction_script else None
+        self._memory = memory
+
+        self._supported_features = 0
+        if self._oscillation_switch:
+            self._supported_features |= SUPPORT_OSCILLATE
+        if self._set_speed_script:
+            self._supported_features |= SUPPORT_SET_SPEED
+        if self._set_direction_script:
+            self._supported_features |= SUPPORT_DIRECTION
+
+        self._state = STATE_UNKNOWN
+        self._speed = STATE_OFF
+        self._oscillating = False
 
     @property
     def should_poll(self):
@@ -65,11 +92,11 @@ class GenericFan(FanEntity):
     @property
     def assumed_state(self):
         """Return true, since state is not maintained by a sensor."""
-        return False
+        return True
 
     @property
     def is_on(self):
-        """Return true if device is on."""
+        """Return true if the fan is on."""
         return self._state == STATE_ON
 
     @property
@@ -79,30 +106,39 @@ class GenericFan(FanEntity):
 
     @property
     def speed_list(self) -> list:
-        """Get the list of available speeds."""
-        return [1, 2, 3]
+        """Return the list of available speeds."""
+        if not self._set_speed_script:
+            return None
+
+        return [STATE_OFF, SPEED_LOW, SPEED_MEDIUM, SPEED_HIGH]
 
     @property
     def supported_features(self) -> int:
-        """Flag supported features."""
-        if self._oscillation_switch:
-            return SUPPORT_OSCILLATE
+        """Return the supported features bitmask."""
+        return self._supported_features
 
     @property
-    def speed(self):
-        """Return the current speed."""
-        if not self.is_on:
-            return 0
-        else:
-            return 1
+    def speed(self) -> str:
+        """Return the current speed of the fan."""
+        if not self._set_speed_script:
+            return None
+        return self._speed
 
     @property
-    def oscillating(self):
-        """Return the oscillation state."""
-        if not self._oscillation_switch:
-            return False
-        else:
-            raise NotImplementedError()
+    def oscillating(self) -> bool:
+        """Return the oscillation state of the fan."""
+        if not self.is_on or not self._oscillation_switch:
+            return None
+
+        return self._oscillating
+    
+    @property
+    def current_direction(self) -> str:
+        """Return the current direction of the fan."""
+        if not self._set_direction_script:
+            return None
+
+        raise NotImplementedError()
 
     @asyncio.coroutine
     def async_added_to_hass(self):
@@ -112,20 +148,24 @@ class GenericFan(FanEntity):
             self._state = state.state == STATE_ON
 
         @callback
-        def power_switch_state_listener(entity, old_state, new_state):
-            """Handle target device state changes."""
+        def switch_state_listener(entity, old_state, new_state):
+            """Handle switches state changes."""
             self.hass.async_add_job(self.async_update_ha_state(True))
 
         @callback
-        def power_switch_startup(event):
+        def generic_fan_startup(event):
             """Update fan on startup."""
             async_track_state_change(
-                self.hass, self._power_switch, power_switch_state_listener)
+                self.hass, self._power_switch, switch_state_listener)
+
+            if self._oscillation_switch:
+                async_track_state_change(
+                    self.hass, self._oscillation_switch, switch_state_listener)
 
             self.hass.async_add_job(self.async_update_ha_state(True))
 
         self.hass.bus.async_listen_once(
-            EVENT_HOMEASSISTANT_START, power_switch_startup)
+            EVENT_HOMEASSISTANT_START, generic_fan_startup)
 
     @asyncio.coroutine
     def async_turn_on(self, speed: str=None) -> None:
@@ -137,6 +177,9 @@ class GenericFan(FanEntity):
 
         if speed:
             yield from self.async_set_speed(speed)
+
+        if not self._memory and self.oscillating:
+            yield from self.oscillate(self._oscillating)
 
     @asyncio.coroutine
     def async_turn_off(self) -> None:
@@ -152,6 +195,9 @@ class GenericFan(FanEntity):
 
         This method is a coroutine.
         """
+        if not self._set_speed_script:
+            raise NotImplementedError()
+
         raise NotImplementedError()
 
     @asyncio.coroutine
@@ -163,14 +209,21 @@ class GenericFan(FanEntity):
         if not self._oscillation_switch:
             raise NotImplementedError()
 
-        raise NotImplementedError()
+        if oscillating:
+            switch.async_turn_on(self.hass, self._oscillation_switch)
+        else:
+            switch.async_turn_off(self.hass, self._oscillation_switch)
 
     @asyncio.coroutine
     def async_update(self) -> None:
         """Update the state."""
         if switch.is_on(self.hass, self._power_switch):
-            logging.error("async_update =====> ON")
             self._state = STATE_ON
         else:
-            logging.error("async_update =====> OFF")
             self._state = STATE_OFF
+
+        if self._oscillation_switch:
+            if switch.is_on(self.hass, self._oscillation_switch):
+                self._oscillating = True
+            else:
+                self._oscillating = False
