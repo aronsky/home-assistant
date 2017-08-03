18d6c6eb7c66a665d20955319332f475073570c4
Lev Aronsky <aronsky@gmail.com>
2017-08-03 17:07:22 +0300

    Continued work on generic fan.

--- homeassistant/components/fan/generic_fan.py
+++ homeassistant/components/fan/generic_fan.py
@@ -8,12 +8,18 @@ import asyncio
 import logging
 import voluptuous as vol
 
+from homeassistant.core import callback
 from homeassistant.components import switch
 from homeassistant.components.fan import (SPEED_LOW, SPEED_MEDIUM, SPEED_HIGH,
                                           FanEntity, SUPPORT_SET_SPEED,
                                           SUPPORT_OSCILLATE, SUPPORT_DIRECTION,
                                           PLATFORM_SCHEMA)
-from homeassistant.const import STATE_OFF, CONF_DEVICES, CONF_NAME
+from homeassistant.const import (STATE_OFF, STATE_ON, CONF_DEVICES, CONF_NAME,
+                                 EVENT_HOMEASSISTANT_START)
+from homeassistant.helpers.event import async_track_state_change
+from homeassistant.helpers.restore_state import async_get_last_state
+from homeassistant.helpers.script import Script
+
 import homeassistant.helpers.config_validation as cv
 
 CONF_POWER_SWITCH = "power"
@@ -37,7 +43,7 @@ def async_setup_platform(hass, config, async_add_devices, discovery_info=None):
         config.get(CONF_POWER_SWITCH),
         config.get(CONF_OSCILLATION_SWITCH)
         )
-    ])
+    ], True)
 
 
 class GenericFan(FanEntity):
@@ -49,6 +55,7 @@ class GenericFan(FanEntity):
         self._name = name
         self._power_switch = power_switch
         self._oscillation_switch = oscillation_switch
+        self._state = None
 
     @property
     def should_poll(self):
@@ -63,7 +70,7 @@ class GenericFan(FanEntity):
     @property
     def is_on(self):
         """Return true if device is on."""
-        return switch.is_on(self.hass, self._power_switch)
+        return self._state == STATE_ON
 
     @property
     def name(self) -> str:
@@ -87,7 +94,7 @@ class GenericFan(FanEntity):
         if not self.is_on:
             return 0
         else:
-            raise NotImplementedError()
+            return 1
 
     @property
     def oscillating(self):
@@ -98,12 +105,35 @@ class GenericFan(FanEntity):
             raise NotImplementedError()
 
     @asyncio.coroutine
+    def async_added_to_hass(self):
+        """Register callbacks."""
+        state = yield from async_get_last_state(self.hass, self.entity_id)
+        if state:
+            self._state = state.state == STATE_ON
+
+        @callback
+        def power_switch_state_listener(entity, old_state, new_state):
+            """Handle target device state changes."""
+            self.hass.async_add_job(self.async_update_ha_state(True))
+
+        @callback
+        def power_switch_startup(event):
+            """Update fan on startup."""
+            async_track_state_change(
+                self.hass, self._power_switch, power_switch_state_listener)
+
+            self.hass.async_add_job(self.async_update_ha_state(True))
+
+        self.hass.bus.async_listen_once(
+            EVENT_HOMEASSISTANT_START, power_switch_startup)
+
+    @asyncio.coroutine
     def async_turn_on(self, speed: str=None) -> None:
         """Turn on the entity.
 
         This method is a coroutine.
         """
-        switch.async_turn_on(self.hass, self._power_switch)
+        switch.turn_on(self.hass, self._power_switch)
 
         if speed:
             yield from self.async_set_speed(speed)
@@ -134,3 +164,13 @@ class GenericFan(FanEntity):
             raise NotImplementedError()
 
         raise NotImplementedError()
+
+    @asyncio.coroutine
+    def async_update(self) -> None:
+        """Update the state."""
+        if switch.is_on(self.hass, self._power_switch):
+            logging.error("async_update =====> ON")
+            self._state = STATE_ON
+        else:
+            logging.error("async_update =====> OFF")
+            self._state = STATE_OFF
